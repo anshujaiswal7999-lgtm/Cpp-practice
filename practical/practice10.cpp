b1013dbf62e1b8cb5d8b40cd3d2c4b64af810939
Anshu Jaiswal <anshujaiswal7999-lgtm>
2026-09-13 23:12:16 +0530

    completed

--- practical/practice10.cpp
+++ practical/practice10.cpp
@@ -19,12 +19,11 @@ class Date{
 };
 int main()
 {
-    cout<<"Date with default value: ";
+cout<<"Date with default value: ";
  Date obj1;
  obj1.display();
-  cout<<"Date with initial value: ";
+cout<<"Date with initial value: ";
  Date obj2(13,9,2026);
 obj2.display();
-
 return 0;
 }
